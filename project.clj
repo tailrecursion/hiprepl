471109c811b2808eeb1bb6420480cadca775019f
Alan Dipert <alan@thinkminimo.com>
2013-02-23 00:30:11 +0000

    clojail

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.2.1"]
                  [clj-http "0.5.8"]
-                 [overtone/at-at "1.1.1"]]
+                 [overtone/at-at "1.1.1"]
+                 [clojail "1.0.4"]]
   :main hiprepl.core)
--- src/hiprepl/core.clj
+++ src/hiprepl/core.clj
@@ -2,12 +2,13 @@
   (:require [clojure.data.json :as json]
             [clj-http.client :as client]
             [clojure.set :as s]
-            [overtone.at-at :refer [mk-pool every]])
-  (:import java.util.concurrent.ScheduledThreadPoolExecutor))
+            [overtone.at-at :refer [mk-pool every]]
+            [clojail.core :refer [sandbox safe-read]]
+            [clojail.testers :refer [secure-tester]]))
 
 (def api-url "http://api.hipchat.com/v1")
 
-(defn rooms
+(defn rooms*
   [token]
   (let [query {:auth_token token :format "json"}]
     (-> (client/get (str api-url "/rooms/list") {:query-params query})
@@ -15,10 +16,14 @@
         json/read-str
         (get "rooms"))))
 
-(defn room-id
+(def rooms (memoize rooms*))
+
+(defn room-id*
   [token room-name]
   (let [rooms (rooms token)]
     (get (first (filter #(= room-name (get % "name")) rooms)) "room_id")))
+
+(def room-id (memoize room-id*))
 
 (defn fetch-recent
   [token room-name]
@@ -32,7 +37,7 @@
         (get "messages")
         set)))
 
-(defn user-info
+(defn user-info*
   [token user-id]
   (let [query {:user_id user-id
                :auth_token token}]
@@ -41,7 +46,9 @@
         json/read-str
         (get "user"))))
 
-(defn message
+(def user-info (memoize user-info*))
+
+(defn send-message
   [token room-name msg]
   (let [query {:room_id (room-id token room-name)
                :from (str "Clojure " (clojure-version))
@@ -52,16 +59,20 @@
         :body
         json/read-str)))
 
+(def secure-sandbox (sandbox secure-tester))
+
 (defn eval-messages
   [token room-name messages]
   (println "Evaluating messages.")
   (doseq [{:strs [message from]} messages
           :when (.startsWith message ",")
-          :let [code (read-string (.substring message 1))
-                return (pr-str (try (eval code) (catch Throwable t (.getMessage t))))
+          :let [code (safe-read (.substring message 1))
+                return (binding [*print-length* 30]
+                         (pr-str (try (secure-sandbox code)
+                                      (catch Throwable t (.getMessage t)))))
                 mention (get (user-info token (get from "user_id")) "mention_name")]]
     (println (format "%s ran %s, got %s" mention (pr-str code) return))
-    (future (message token room-name (format "@%s %s" mention return)))))
+    (future (send-message token room-name (format "@%s %s" mention return)))))
 
 (defn -main
   [auth-token room-name]
@@ -69,7 +80,7 @@
                          :eval #{}})
         pool (mk-pool)]
     (add-watch messages ::eval #(eval-messages auth-token room-name (:eval %4)))
-    (every 5000
+    (every 4000
            (fn []
              (println "Polling...")
              (send-off messages (fn [{:keys [prev]}]
